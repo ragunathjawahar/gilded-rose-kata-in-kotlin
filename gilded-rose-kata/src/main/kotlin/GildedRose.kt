230b91bbcf59cc38b6dcaad35c614f7f9ba4606c
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-16 08:58:16 +0530

    GildedRose, replace 'for' loop with 'forEach' function.

--- gilded-rose-kata/src/main/kotlin/GildedRose.kt
+++ gilded-rose-kata/src/main/kotlin/GildedRose.kt
@@ -4,54 +4,54 @@ class GildedRose(val items: List<Item>) {
   constructor(item: Item) : this(listOf(item))
 
   fun updateQuality() {
-    for (i in 0..items.size-1) {
-      if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
-        if (items[i].quality > 0) {
-          if (items[i].name != SULFURAS) {
-            items[i].quality = items[i].quality - 1
+    items.forEach { item ->
+      if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES) {
+        if (item.quality > 0) {
+          if (item.name != SULFURAS) {
+            item.quality = item.quality - 1
           }
         }
       }
       else {
-        if (items[i].quality < MAX_QUALITY) {
-          items[i].quality = items[i].quality + 1
+        if (item.quality < MAX_QUALITY) {
+          item.quality = item.quality + 1
 
-          if (items[i].name == BACKSTAGE_PASSES) {
-            if (items[i].sellIn < 11) {
-              if (items[i].quality < MAX_QUALITY) {
-                items[i].quality = items[i].quality + 1
+          if (item.name == BACKSTAGE_PASSES) {
+            if (item.sellIn < 11) {
+              if (item.quality < MAX_QUALITY) {
+                item.quality = item.quality + 1
               }
             }
 
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < MAX_QUALITY) {
-                items[i].quality = items[i].quality + 1
+            if (item.sellIn < 6) {
+              if (item.quality < MAX_QUALITY) {
+                item.quality = item.quality + 1
               }
             }
           }
         }
       }
 
-      if (items[i].name != SULFURAS) {
-        items[i].sellIn = items[i].sellIn - 1
+      if (item.name != SULFURAS) {
+        item.sellIn = item.sellIn - 1
       }
 
-      if (items[i].sellIn < 0) {
-        if (items[i].name != AGED_BRIE) {
-          if (items[i].name != BACKSTAGE_PASSES) {
-            if (items[i].quality > 0) {
-              if (items[i].name != SULFURAS) {
-                items[i].quality = items[i].quality - 1
+      if (item.sellIn < 0) {
+        if (item.name != AGED_BRIE) {
+          if (item.name != BACKSTAGE_PASSES) {
+            if (item.quality > 0) {
+              if (item.name != SULFURAS) {
+                item.quality = item.quality - 1
               }
             }
           }
           else {
-            items[i].quality = items[i].quality - items[i].quality
+            item.quality = item.quality - item.quality
           }
         }
         else {
-          if (items[i].quality < MAX_QUALITY) {
-            items[i].quality = items[i].quality + 1
+          if (item.quality < MAX_QUALITY) {
+            item.quality = item.quality + 1
           }
         }
       }
