976f49d48accc0dd6fad6eedbc6de8c94724b8e0
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-17 03:10:58 +0530

    GildedRose, extract 'updateQuality(Item)' function.

--- gilded-rose-kata/src/main/kotlin/GildedRose.kt
+++ gilded-rose-kata/src/main/kotlin/GildedRose.kt
@@ -4,55 +4,54 @@ class GildedRose(val items: List<Item>) {
   constructor(item: Item) : this(listOf(item))
 
   fun updateQuality() {
-    items.forEach { item ->
-      if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES) {
-        if (item.quality > 0) {
-          if (item.name != SULFURAS) {
-            item.quality = item.quality - 1
+    items.forEach(::updateQuality)
+  }
+
+  private fun updateQuality(item: Item) {
+    if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES) {
+      if (item.quality > 0) {
+        if (item.name != SULFURAS) {
+          item.quality = item.quality - 1
+        }
+      }
+    } else {
+      if (item.quality < MAX_QUALITY) {
+        item.quality = item.quality + 1
+
+        if (item.name == BACKSTAGE_PASSES) {
+          if (item.sellIn < 11) {
+            if (item.quality < MAX_QUALITY) {
+              item.quality = item.quality + 1
+            }
+          }
+
+          if (item.sellIn < 6) {
+            if (item.quality < MAX_QUALITY) {
+              item.quality = item.quality + 1
+            }
           }
         }
       }
-      else {
+    }
+
+    if (item.name != SULFURAS) {
+      item.sellIn = item.sellIn - 1
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name != AGED_BRIE) {
+        if (item.name != BACKSTAGE_PASSES) {
+          if (item.quality > 0) {
+            if (item.name != SULFURAS) {
+              item.quality = item.quality - 1
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality
+        }
+      } else {
         if (item.quality < MAX_QUALITY) {
           item.quality = item.quality + 1
-
-          if (item.name == BACKSTAGE_PASSES) {
-            if (item.sellIn < 11) {
-              if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (item.name != SULFURAS) {
-        item.sellIn = item.sellIn - 1
-      }
-
-      if (item.sellIn < 0) {
-        if (item.name != AGED_BRIE) {
-          if (item.name != BACKSTAGE_PASSES) {
-            if (item.quality > 0) {
-              if (item.name != SULFURAS) {
-                item.quality = item.quality - 1
-              }
-            }
-          }
-          else {
-            item.quality = item.quality - item.quality
-          }
-        }
-        else {
-          if (item.quality < MAX_QUALITY) {
-            item.quality = item.quality + 1
-          }
         }
       }
     }
