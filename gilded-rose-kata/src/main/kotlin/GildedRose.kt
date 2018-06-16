48ca4028bb068cab4280df1ceaecceb6dee4d995
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-17 03:50:23 +0530

    GildedRose, trap normal item execution path.

--- gilded-rose-kata/src/main/kotlin/GildedRose.kt
+++ gilded-rose-kata/src/main/kotlin/GildedRose.kt
@@ -30,61 +30,23 @@ class GildedRose(val items: List<Item>) {
     }
   }
 
+  private fun sulfurasUpdateQuality(item: Item) {
+    /* do nothing */
+  }
+
+  private fun normalUpdateQuality(item: Item) {
+    item.sellIn -= 1
+    if (item.quality > 0) {
+      item.quality -= if (item.sellIn > 0) 1 else 2
+    }
+  }
+
   private fun updateQuality(item: Item) {
-    if (item.name == AGED_BRIE) {
-      agedBrieUpdateQuality(item)
-      return
-    } else if (item.name == BACKSTAGE_PASSES) {
-      backstagePassesUpdateQuality(item)
-      return
-    }
-
-    if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES) {
-      if (item.quality > 0) {
-        if (item.name != SULFURAS) {
-          item.quality = item.quality - 1
-        }
-      }
-    } else {
-      if (item.quality < MAX_QUALITY) {
-        item.quality = item.quality + 1
-
-        if (item.name == BACKSTAGE_PASSES) {
-          if (item.sellIn < 11) {
-            if (item.quality < MAX_QUALITY) {
-              item.quality = item.quality + 1
-            }
-          }
-
-          if (item.sellIn < 6) {
-            if (item.quality < MAX_QUALITY) {
-              item.quality = item.quality + 1
-            }
-          }
-        }
-      }
-    }
-
-    if (item.name != SULFURAS) {
-      item.sellIn = item.sellIn - 1
-    }
-
-    if (item.sellIn < 0) {
-      if (item.name != AGED_BRIE) {
-        if (item.name != BACKSTAGE_PASSES) {
-          if (item.quality > 0) {
-            if (item.name != SULFURAS) {
-              item.quality = item.quality - 1
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality
-        }
-      } else {
-        if (item.quality < MAX_QUALITY) {
-          item.quality = item.quality + 1
-        }
-      }
+    when {
+      item.name == AGED_BRIE -> agedBrieUpdateQuality(item)
+      item.name == BACKSTAGE_PASSES -> backstagePassesUpdateQuality(item)
+      item.name == SULFURAS -> sulfurasUpdateQuality(item)
+      else -> normalUpdateQuality(item)
     }
   }
 }
