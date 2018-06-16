9c6c79f314d986f66beb7d6decf133be9aa6a4f2
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-16 07:05:16 +0530

    Defer "Conjured" item till we add tests.

--- gilded-rose-kata/src/main/kotlin/Main.kt
+++ gilded-rose-kata/src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ fun main(args: Array<String>) {
       Item("Aged Brie", 2, 0),
       Item("Elixir of the Mongoose", 5, 7),
       Item("Sulfuras, Hand of Ragnaros", 0, 80),
-      Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-      Item("Conjured Mana Cake", 15, 20)
+      Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+      // Item("Conjured Mana Cake", 15, 20)
   )
 
   val gildedRose = GildedRose(items)
