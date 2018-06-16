974cb2675b0dbe42aacef200b579b2fe8e2ff4c5
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-17 03:45:03 +0530

    GildedRose, trap backstage passes execution path.

--- gilded-rose-kata/src/main/kotlin/GildedRose.kt
+++ gilded-rose-kata/src/main/kotlin/GildedRose.kt
@@ -15,9 +15,27 @@ class GildedRose(val items: List<Item>) {
     }
   }
 
+  private fun backstagePassesUpdateQuality(item: Item) {
+    if (item.sellIn >= 0 && item.quality < MAX_QUALITY) {
+      item.quality += when {
+        item.sellIn in 0..5 -> 3
+        item.sellIn in 6..10 -> 2
+        else -> 1
+      }
+    }
+
+    item.sellIn -= 1
+    if (item.sellIn < 0) {
+      item.quality = 0
+    }
+  }
+
   private fun updateQuality(item: Item) {
     if (item.name == AGED_BRIE) {
       agedBrieUpdateQuality(item)
+      return
+    } else if (item.name == BACKSTAGE_PASSES) {
+      backstagePassesUpdateQuality(item)
       return
     }
 
