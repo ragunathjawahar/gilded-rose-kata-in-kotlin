5ec686fb5aaee44c884d441e08bd38a9632ed463
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-16 06:25:19 +0530

    Project, use 2 spaces for indentation.

--- gilded-rose-kata/src/main/kotlin/Item.kt
+++ gilded-rose-kata/src/main/kotlin/Item.kt
@@ -1,8 +1,7 @@
 data class Item(
-        val name: String,
-        var sellIn: Int,
-        var quality: Int) {
-
-    override fun toString() = "$name / sell in $sellIn / quality of $quality"
-
+    val name: String,
+    var sellIn: Int,
+    var quality: Int
+) {
+  override fun toString() = "$name / sell in $sellIn / quality of $quality"
 }
