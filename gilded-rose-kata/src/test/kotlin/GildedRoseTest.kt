fa96dc565a1730b81b3449ef1e9c0b23bb5c1611
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-06-18 07:39:50 +0530

    GildedRoseTest, improve readability.

--- gilded-rose-kata/src/test/kotlin/GildedRoseTest.kt
+++ gilded-rose-kata/src/test/kotlin/GildedRoseTest.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import store.CONJURED_MANA_CAKE
 import store.DEXTERITY_VEST
 import store.ELIXIR_OF_THE_MONGOOSE
+import store.Item
 import store.Item.Companion.MAX_QUALITY
 import store.ItemFactory.Companion.agedBrie
 import store.ItemFactory.Companion.backstagePasses
@@ -30,6 +31,8 @@ class GildedRoseTest {
 
     // then
     items.forEach {
+      assertThat(it.sellIn)
+          .isAtMost(0)
       assertThat(it.quality)
           .isAtLeast(0)
     }
@@ -38,131 +41,131 @@ class GildedRoseTest {
   @Test
   fun `normal item's quality degrades by 1`() {
     // given
-    val dragonEgg = normal("Dragon Egg", 10, 40)
+    val dragonEgg = normal("Dragon Egg", sellIn = 10, quality = 40)
 
-    // when
-    dragonEgg.update()
-
-    // then
-    assertThat(dragonEgg.sellIn).isEqualTo(9)
-    assertThat(dragonEgg.quality).isEqualTo(39)
+    // when & then
+    assertUpdate(
+        dragonEgg,
+        expectedSellIn = 9,
+        expectedQuality = 39
+    )
   }
 
   @Test
   fun `normal item quality does not degrade below 0`() {
     // given
-    val dragonEgg = normal("Dragon Egg", 0, 0)
+    val dragonEgg = normal("Dragon Egg", sellIn = 0, quality = 0)
 
-    // when
-    dragonEgg.update()
-
-    // then
-    assertThat(dragonEgg.sellIn).isEqualTo(-1)
-    assertThat(dragonEgg.quality).isEqualTo(0)
+    // when & then
+    assertUpdate(
+        dragonEgg,
+        expectedSellIn = -1,
+        expectedQuality = 0
+    )
   }
 
   @Test
   fun `normal item's quality degrades by 2 once the sell-in date passes`() {
     // given
-    val dragonEgg = normal("Dragon Egg", 0, 20)
+    val dragonEgg = normal("Dragon Egg", sellIn = 0, quality = 20)
 
-    // when
-    dragonEgg.update()
-
-    // then
-    assertThat(dragonEgg.sellIn).isEqualTo(-1)
-    assertThat(dragonEgg.quality).isEqualTo(18)
+    // when & then
+    assertUpdate(
+        dragonEgg,
+        expectedSellIn = -1,
+        expectedQuality = 18
+    )
   }
 
   @Test
   fun `normal item's quality should not be less than 0 when it degrades by 2 after the sell-in date`() {
     // given
-    val dragonEgg = normal("Dragon Egg", 0, 1)
+    val dragonEgg = normal("Dragon Egg", sellIn = 0, quality = 1)
 
-    // when
-    dragonEgg.update()
-
-    // then
-    assertThat(dragonEgg.sellIn).isEqualTo(-1)
-    assertThat(dragonEgg.quality).isEqualTo(0)
+    // when & then
+    assertUpdate(
+        dragonEgg,
+        expectedSellIn = -1,
+        expectedQuality = 0
+    )
   }
 
   @Test
   fun `aged brie's quality increases by 1`() {
     // given
-    val agedBrie = agedBrie(10, 10)
+    val agedBrie = agedBrie(sellIn = 10, quality = 10)
 
-    // when
-    agedBrie.update()
-
-    // then
-    assertThat(agedBrie.sellIn).isEqualTo(9)
-    assertThat(agedBrie.quality).isEqualTo(11)
+    // when & then
+    assertUpdate(
+        agedBrie,
+        expectedSellIn = 9,
+        expectedQuality = 11
+    )
   }
 
   @Test
   fun `aged brie's quality increases by 2 beyond the sell-in date`() {
     // given
-    val agedBrie = agedBrie(0, 20)
+    val agedBrie = agedBrie(sellIn = 0, quality = 20)
 
-    // when
-    agedBrie.update()
-
-    // then
-    assertThat(agedBrie.sellIn).isEqualTo(-1)
-    assertThat(agedBrie.quality).isEqualTo(22)
+    // when & then
+    assertUpdate(
+        agedBrie,
+        expectedSellIn = -1,
+        expectedQuality = 22
+    )
   }
 
   @Test
   fun `aged brie's quality increase even if it is 0`() {
     // given
-    val agedBrie = agedBrie(10, 0)
+    val agedBrie = agedBrie(sellIn = 10, quality = 0)
 
-    // when
-    agedBrie.update()
-
-    // then
-    assertThat(agedBrie.sellIn).isEqualTo(9)
-    assertThat(agedBrie.quality).isEqualTo(1)
+    // when & then
+    assertUpdate(
+        agedBrie,
+        expectedSellIn = 9,
+        expectedQuality = 1
+    )
   }
 
   @Test
   fun `aged brie's quality does not increase beyond max quality before the sell-in date`() {
     // given
-    val agedBrie = agedBrie(10, MAX_QUALITY)
+    val agedBrie = agedBrie(sellIn = 10, quality = MAX_QUALITY)
 
-    // when
-    agedBrie.update()
-
-    // then
-    assertThat(agedBrie.sellIn).isEqualTo(9)
-    assertThat(agedBrie.quality).isEqualTo(MAX_QUALITY)
+    // when & then
+    assertUpdate(
+        agedBrie,
+        expectedSellIn = 9,
+        expectedQuality = MAX_QUALITY
+    )
   }
 
   @Test
   fun `aged brie's quality does not increase beyond max quality after the sell-in date`() {
     // given
-    val agedBrie = agedBrie(-5, 49)
+    val agedBrie = agedBrie(sellIn = -5, quality = 49)
 
-    // when
-    agedBrie.update()
-
-    // then
-    assertThat(agedBrie.sellIn).isEqualTo(-6)
-    assertThat(agedBrie.quality).isEqualTo(MAX_QUALITY)
+    // when & then
+    assertUpdate(
+        agedBrie,
+        expectedSellIn = -6,
+        expectedQuality = MAX_QUALITY
+    )
   }
 
   @Test
   fun `aged brie's quality is never negative`() {
     // given
-    val agedBrie = agedBrie(0, 0)
+    val agedBrie = agedBrie(sellIn = 0, quality = 0)
 
-    // when
-    agedBrie.update()
-
-    // then
-    assertThat(agedBrie.sellIn).isEqualTo(-1)
-    assertThat(agedBrie.quality).isEqualTo(2)
+    // when & then
+    assertUpdate(
+        agedBrie,
+        expectedSellIn = -1,
+        expectedQuality = 2
+    )
   }
 
   @Test
@@ -185,33 +188,33 @@ class GildedRoseTest {
   @Test
   fun `backstage passes, quality increases by 1 when there are more than 10 days`() {
     // given
-    val backstagePasses = backstagePasses(15, 20)
+    val backstagePasses = backstagePasses(sellIn = 15, quality = 20)
 
-    // when
-    backstagePasses.update()
-
-    // then
-    assertThat(backstagePasses.sellIn).isEqualTo(14)
-    assertThat(backstagePasses.quality).isEqualTo(21)
+    // when & then
+    assertUpdate(
+        backstagePasses,
+        expectedSellIn = 14,
+        expectedQuality = 21
+    )
   }
 
   @Test
   fun `backstage passes, quality increases before the concert event if it is already 0`() {
     // given
-    val backstagePasses = backstagePasses(11, 0)
+    val backstagePasses = backstagePasses(sellIn = 11, quality = 0)
 
-    // when
-    backstagePasses.update()
-
-    // then
-    assertThat(backstagePasses.sellIn).isEqualTo(10)
-    assertThat(backstagePasses.quality).isEqualTo(1)
+    // when & then
+    assertUpdate(
+        backstagePasses,
+        expectedSellIn = 10,
+        expectedQuality = 1
+    )
   }
 
   @Test
   fun `backstage passes, quality increases by 2 between 10 to 6 days`() {
     // given
-    val backstagePasses = backstagePasses(10, 1)
+    val backstagePasses = backstagePasses(sellIn = 10, quality = 1)
 
     // when
     for (i in 10 downTo 6) {
@@ -226,7 +229,7 @@ class GildedRoseTest {
   @Test
   fun `backstage passes, quality increases by 3 between 5 to 1 days`() {
     // given
-    val backstagePasses = backstagePasses(5, 1)
+    val backstagePasses = backstagePasses(sellIn = 5, quality = 1)
 
     // when
     for (i in 5 downTo 1) {
@@ -242,62 +245,62 @@ class GildedRoseTest {
   @Test
   fun `backstage passes, quality does not increase beyond max quality if sell-in is more than 10 days`() {
     // given
-    val backstagePasses = backstagePasses(24, MAX_QUALITY)
+    val backstagePasses = backstagePasses(sellIn = 24, quality = MAX_QUALITY)
 
-    // when
-    backstagePasses.update()
-
-    // then
-    assertThat(backstagePasses.sellIn).isEqualTo(23)
-    assertThat(backstagePasses.quality).isEqualTo(MAX_QUALITY)
+    // when & then
+    assertUpdate(
+        backstagePasses,
+        expectedSellIn = 23,
+        expectedQuality = MAX_QUALITY
+    )
   }
 
   @Test
   fun `backstage passes, quality does not increase beyond max quality between 10 to 6 days`() {
     // given
-    val backstagePasses = backstagePasses(10, MAX_QUALITY)
+    val backstagePasses = backstagePasses(sellIn = 10, quality = MAX_QUALITY)
 
-    // when
-    backstagePasses.update()
-
-    // then
-    assertThat(backstagePasses.sellIn).isEqualTo(9)
-    assertThat(backstagePasses.quality).isEqualTo(MAX_QUALITY)
+    // when & then
+    assertUpdate(
+        backstagePasses,
+        expectedSellIn = 9,
+        expectedQuality = MAX_QUALITY
+    )
   }
 
   @Test
   fun `backstage passes, quality does not increase beyond max quality between 5 to 1 days`() {
     // given
-    val backstagePasses = backstagePasses(4, MAX_QUALITY)
+    val backstagePasses = backstagePasses(sellIn = 4, quality = MAX_QUALITY)
 
-    // when
-    backstagePasses.update()
-
-    // then
-    assertThat(backstagePasses.sellIn).isEqualTo(3)
-    assertThat(backstagePasses.quality).isEqualTo(MAX_QUALITY)
+    // when & then
+    assertUpdate(
+        backstagePasses,
+        expectedSellIn = 3,
+        expectedQuality = MAX_QUALITY
+    )
   }
 
   @Test
   fun `backstage passes, quality falls down to zero after the concert`() {
     // given
-    val backstagePasses = backstagePasses(0, MAX_QUALITY)
+    val backstagePasses = backstagePasses(sellIn = 0, quality = MAX_QUALITY)
 
-    // when
-    backstagePasses.update()
-
-    // then
-    assertThat(backstagePasses.sellIn).isEqualTo(-1)
-    assertThat(backstagePasses.quality).isEqualTo(0)
+    // when & then
+    assertUpdate(
+        backstagePasses,
+        expectedSellIn = -1,
+        expectedQuality = 0
+    )
   }
 
   @Test
-  fun `backstage passes, quality does not degrade below 0 after the concert`() {
+  fun `backstage passes quality does not degrade below 0 after the concert`() {
     // given
-    val backstagePasses = backstagePasses(2, MAX_QUALITY)
+    val backstagePasses = backstagePasses(sellIn = 2, quality = MAX_QUALITY)
 
     // when
-    for (i in 2 downTo -3) {
+    for (i in 1..6) {
       backstagePasses.update()
     }
 
@@ -307,41 +310,52 @@ class GildedRoseTest {
   }
 
   @Test
-  fun `conjured mana cake, degrades in quality by 2 before sell-in date`() {
+  fun `conjured item quality degrades by 2 before sell-in date`() {
     // given
-    val manaCake = conjured(CONJURED_MANA_CAKE, 10, 12)
+    val manaCake = conjured(CONJURED_MANA_CAKE, sellIn = 10, quality = 12)
 
-    // when
-    manaCake.update()
-
-    // then
-    assertThat(manaCake.sellIn).isEqualTo(9)
-    assertThat(manaCake.quality).isEqualTo(10)
+    // when & then
+    assertUpdate(
+        manaCake,
+        expectedSellIn = 9,
+        expectedQuality = 10
+    )
   }
 
   @Test
-  fun `conjured mana cake, degrades in quality by 4 after the sell-in date`() {
+  fun `conjured item quality degrades by 4 after the sell-in date`() {
     // given
-    val manaCake = conjured(CONJURED_MANA_CAKE, 0, 10)
+    val manaCake = conjured(CONJURED_MANA_CAKE, sellIn = 0, quality = 10)
 
-    // when
-    manaCake.update()
-
-    // then
-    assertThat(manaCake.sellIn).isEqualTo(-1)
-    assertThat(manaCake.quality).isEqualTo(6)
+    // when & then
+    assertUpdate(
+        manaCake,
+        expectedSellIn = -1,
+        expectedQuality = 6
+    )
   }
 
   @Test
-  fun `conjured mana cake, quality does not go below 0`() {
+  fun `conjured item quality does not degrade below 0`() {
     // given
-    val manaCake = conjured(CONJURED_MANA_CAKE, 0, 0)
+    val manaCake = conjured(CONJURED_MANA_CAKE, sellIn = 0, quality = 0)
 
-    // when
-    manaCake.update()
+    // when & then
+    assertUpdate(
+        manaCake,
+        expectedSellIn = -1,
+        expectedQuality = 0
+    )
+  }
 
-    // then
-    assertThat(manaCake.sellIn).isEqualTo(-1)
-    assertThat(manaCake.quality).isEqualTo(0)
+  private fun assertUpdate(
+      item: Item,
+      expectedSellIn: Int,
+      expectedQuality: Int
+  ) {
+    item.update()
+
+    assertThat(item.sellIn).isEqualTo(expectedSellIn)
+    assertThat(item.quality).isEqualTo(expectedQuality)
   }
 }
